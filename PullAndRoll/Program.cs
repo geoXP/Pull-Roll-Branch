08c9a74a039f14c47354d7481263f262c67bbf91
ShimuSourav <rdfox.007@gmail.com>
2019-11-22 02:40:31 +0600

    console app

---
+++ PullAndRoll/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PullAndRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Push > Pull > RollBack > Branching");
+            Console.ReadKey();
+        }
+    }
+}
